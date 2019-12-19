63471b4c2d439b4182353d00409943f2f4d6ced9
Andydecard <andydecard@yandex.ru>
2019-12-19 17:29:53 +0300

    Lessson_4_modify

--- Lesson_4/main.js
+++ Lesson_4/main.js
@@ -5,15 +5,15 @@ const regexp2 = /test/gmi;
 console.log(regexp.test(str));
 
 
-const str1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisl nisl, bibendum in aliquet eu, ' +
+const str1 = 'Lorem ipsum dolor sit amet, \'consectetur\' adipiscing elit. Nulla nisl nisl, bibendum in aliquet eu, ' +
     'blandit a massa. Donec placerat viverra tortor, ut ultricies ante sollicitudin non. Mauris non pretium velit.' +
     ' Vivamus blandit bibendum vulputate. Sed non mollis velit, vitae faucibus nulla. Integer at nunc ut ex vehicula' +
     ' ullamcorper. Donec ornare nisi massa, id porta odio consequat non. Praesent nec vehicula dolor. Ut ' +
     'rhoncus efficitur risus vitae pulvinar. Maecenas luctus lorem at odio pharetra maximus. Nullam pulvinar' +
     ' urna vitae tempor placerat. Etiam in posuere est, nec sagittis elit. Sed rhoncus velit id congue suscipit. ' +
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu lacinia orci.';
-const regexp3 = /b/gmi;
-
+const regexp3 = /\'/gmi;
+console.log(str1.replace(regexp3, '"'));
 
 // console.log(str1.match(regexp3));
 const str2 = str1.replace(regexp3, '+');
